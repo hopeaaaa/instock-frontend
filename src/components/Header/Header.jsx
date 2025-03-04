143a415787f3e4f021f27b4eac8ea9a93e57f450
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-03-04 14:42:38 -0700

    Header Function Complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import inStockLogo from "../../assets/logo/InStock-Logo_1x.png";
 import "./Header.scss";
 
@@ -6,18 +6,26 @@ function Header() {
   return (
     <div className="header">
       <div className="header__instock">
-        <a href="/" className="temporary-link">
+        <NavLink to="/" className="temporary-link">
           <img
             src={inStockLogo}
             alt="InStock Logo"
             className="header__hero-image"
           />
-        </a>
+        </NavLink>
       </div>
-      <div className="header__redirect">
-        <h2 className="header__warehouse">Warehouses</h2>
-        <h2 className="header__inventory">Inventory</h2>
-      </div>
+      <ul className="header__nav-bar">
+        <li className="header__warehouse">
+          <NavLink to="/" className="header__nav-link">
+            Warehouses
+          </NavLink>
+        </li>
+        <li className="header__inventory">
+          <NavLink to="/inventory" className="header__nav-link">
+            Inventory
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
