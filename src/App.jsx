9a8504c84c039b7d24627d0acba3f715bffe940d
Kristyna Cabanova <kristynacabanova@gmail.com>
2025-03-04 11:27:35 -0800

    add basic routes and more pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import WarehousesPage from "./pages/Warehouses/WarehousesPage.jsx";
+import InventoryPage from "./pages/Inventory/InventoryPage.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<WarehousesPage />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Inventory/InventoryPage.jsx
@@ -0,0 +1,7 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function InventoryPage() {
+  return <div></div>;
+}
+
+export default InventoryPage;
