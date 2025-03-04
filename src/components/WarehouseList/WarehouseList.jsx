aa297f83d119d2d0b75a5f27dc31b4f9425f855d
Hope Akello <hope.akello@hotmail.com>
2025-03-04 10:14:28 -0800

    first commit

---
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { useState } from "react";
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
+return(
+
+)
+
+export default WarehouseList
