2305f2f498b51bbbb94a006c47a6441f5975b345
Sumer Bhaidani <sumerbhaidani1@gmail.com>
2025-03-05 14:06:10 -0700

    Warehouse Detail Layout Complete

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import BackButton from "../../assets/icons/arrow_back-24px.svg";
 import EditButton from "../../assets/icons/edit-white-24px.svg";
 import RightArrow from "../../assets/icons/chevron_right-24px.svg";
+import EditItem from "../../assets/icons/edit-24px.svg";
+import DeleteItem from "../../assets/icons/delete_outline-24px.svg";
 import "./WarehouseDetails.scss";
 
 function WarehouseDetails() {
@@ -50,9 +52,9 @@ function WarehouseDetails() {
           </div>
         </div>
       </div>
+      {/* map function somewhere here */}
       <div className="warehouse-details__inventory">
-        {/* map function somewhere here */}
-        <div className="warehouse-details__item">
+        <div className="warehouse-details__item warehouse-details__item-info">
           <h5 className="warehouse-details__label--mobile">INVENTORY ITEM</h5>
           <Link to="/" className="warehouse-details__item-redirect">
             <p className="warehouse-details__item-input">item_name</p>
@@ -63,7 +65,72 @@ function WarehouseDetails() {
             />
           </Link>
         </div>
+        <div className="warehouse-details__category warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">CATEGORY</h5>
+          <p className="warehouse-details__item-input">category</p>
+        </div>
+        <div className="warehouse-details__status warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">STATUS</h5>
+          <p className="warehouse-details__item-input">status</p>
+        </div>
+        <div className="warehouse-details__quantity warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">QUANTITY</h5>
+          <p className="warehouse-details__item-input">quantity</p>
+        </div>
+
+        <div className="warehouse-details__actions">
+          <img
+            src={DeleteItem}
+            alt="Delete this Item Image"
+            className="warehouse-details__item-delete"
+          />
+          <img
+            src={EditItem}
+            alt="Edit this Item Image"
+            className="warehouse-details__item-edit"
+          />
+        </div>
       </div>
+      {/* comment out below as its duplicate */}
+      <div className="warehouse-details__inventory">
+        <div className="warehouse-details__item warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">INVENTORY ITEM</h5>
+          <Link to="/" className="warehouse-details__item-redirect">
+            <p className="warehouse-details__item-input">item_name</p>
+            <img
+              src={RightArrow}
+              alt="Arrow to Navigate to Item Detail Page"
+              className="warehouse-details__arrow-image"
+            />
+          </Link>
+        </div>
+        <div className="warehouse-details__category warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">CATEGORY</h5>
+          <p className="warehouse-details__item-input">category</p>
+        </div>
+        <div className="warehouse-details__status warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">STATUS</h5>
+          <p className="warehouse-details__item-input">status</p>
+        </div>
+        <div className="warehouse-details__quantity warehouse-details__item-info">
+          <h5 className="warehouse-details__label--mobile">QUANTITY</h5>
+          <p className="warehouse-details__item-input">quantity</p>
+        </div>
+
+        <div className="warehouse-details__actions">
+          <img
+            src={DeleteItem}
+            alt="Delete this Item Image"
+            className="warehouse-details__item-delete"
+          />
+          <img
+            src={EditItem}
+            alt="Edit this Item Image"
+            className="warehouse-details__item-edit"
+          />
+        </div>
+      </div>
+      {/* comment out above as its duplicate */}
     </div>
   );
 }
