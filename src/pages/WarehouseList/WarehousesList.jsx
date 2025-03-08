3bad56ac5bb68bc5bded15cc1b5236a42b16a1b5
Manuel Louie Gayao <louiemanuel238@gmail.com>
2025-03-08 12:01:42 -0700

    edit warehouse comp & minor styling

--- src/pages/WarehouseList/WarehousesList.jsx
+++ src/pages/WarehouseList/WarehousesList.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import WarehouseDeleteModal from "../../components/WarehouseDeleteModal/WarehouseDeleteModal";
 import WarehouseListComponent from "../../components/WarehouseListComponent/WarehouseListComponent";
 import { useParams } from "react-router-dom";
+import loadWarehouses from "../../utils/FetchWarehousesList/FetchWarehousesList";
 
 function WarehouseList() {
   const { warehouseData } = useParams();
   const [showDelete, setShowDelete] = useState(false);
   const [clickedItem, setClickedItem] = useState(null);
-
+  const [warehouseList, setWarehouseList] = useState([]);
   const handleDeleteClick = (item) => {
     setClickedItem(item);
     setShowDelete(!showDelete);
   };
+
+  useEffect(() => {
+    const getWarehouses = async () => {
+      const response = await loadWarehouses("/api/warehouses");
+      setWarehouseList(response);
+    };
+    getWarehouses();
+  }, []);
 
   return (
     <>
@@ -19,9 +28,10 @@ function WarehouseList() {
         <WarehouseDeleteModal
           setShowDelete={setShowDelete}
           item={clickedItem}
+          warehouseList={warehouseList} setWarehouseList={setWarehouseList}
         />
       )}
-      <WarehouseListComponent warehouseData={warehouseData} handleDeleteClick={handleDeleteClick}/>
+      <WarehouseListComponent warehouseData={warehouseData} handleDeleteClick={handleDeleteClick} WarehouseList={warehouseList}/>
     </>
   );
 }
