0d1dd0b91b4d63eaf2aaafb62dd95abc2e1e4bcf
Hope Akello <hope.akello@hotmail.com>
2025-03-09 08:55:56 -0700

    header styling fix

--- src/components/WarehouseListComponent/WarehouseListComponent.jsx
+++ src/components/WarehouseListComponent/WarehouseListComponent.jsx
@@ -26,20 +26,73 @@ function WarehouseListComponent({ handleDeleteClick, WarehouseList }) {
           <div className="warehouse-list__details">
             <div className="warehouse-list__detail">
               {" "}
-              <h4 className="warehouse-list__column-header">Warehouse</h4>
+              <h4 className="warehouse-list__column-header">
+                Warehouse{" "}
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
+                    fill="#5C667E"
+                  />
+                </svg>
+              </h4>
             </div>
             <div className="warehouse-list__detail">
-              {" "}
-              <h4 className="warehouse-list__column-header">Address</h4>
+              <h4 className="warehouse-list__column-header">
+                Address{" "}
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
+                    fill="#5C667E"
+                  />
+                </svg>
+              </h4>
             </div>
           </div>
           <div className="warehouse-list__details">
             <div className="warehouse-list__detail">
-              <h4 className="warehouse-list__column-header">Contact Name</h4>
+              <h4 className="warehouse-list__column-header">
+                Contact Name{" "}
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
+                    fill="#5C667E"
+                  />
+                </svg>
+              </h4>
             </div>
             <div className="warehouse-list__detail">
               <h4 className="warehouse-list__column-header">
-                Contact Information
+                Contact Information{" "}
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
+                    fill="#5C667E"
+                  />
+                </svg>
               </h4>
             </div>
           </div>
@@ -128,34 +181,34 @@ function WarehouseListComponent({ handleDeleteClick, WarehouseList }) {
           </div>
           <div className="warehouse-list__hidden">
             <button onClick={() => handleDeleteClick(item)}>
-            <svg
-              className="warehouse-list__icon"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z"
-                fill="#C94515"
-              />
-            </svg>
+              <svg
+                className="warehouse-list__icon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z"
+                  fill="#C94515"
+                />
+              </svg>
             </button>
             <Link to={`/warehouse/${item.id}/edit`}>
-            <svg
-              className="warehouse-list__icon"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
-                fill="#2E66E6"
-              />
-            </svg>
+              <svg
+                className="warehouse-list__icon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
+                  fill="#2E66E6"
+                />
+              </svg>
             </Link>
           </div>
         </div>
